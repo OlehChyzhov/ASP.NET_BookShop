5224b60ff22a07afa93523501cd3de9e41d3bae1
Олег Чижов <oleh.chyzhov@lnu.edu.ua>
2024-07-04 18:26:43 +0300

    Add project files.

---
+++ ASP.NET_BookShop/Data/ShopContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_BookShop.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options): base(options) {}
+    }
+}
