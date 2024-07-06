7c41e39c847cda025e023a7bb76baf064987b196
Олег Чижов <oleh.chyzhov@lnu.edu.ua>
2024-07-06 18:31:05 +0300

    Category creation finished

--- ASP.NET_BookShop/Controllers/CategoryController.cs
+++ ASP.NET_BookShop/Controllers/CategoryController.cs
@@ -16,5 +16,53 @@ namespace ASP.NET_BookShop.Controllers
             List<Category> all_categories = shop_context.Categories.ToList();
             return View(all_categories);
         }
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString()) ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            if (ModelState.IsValid == false || category.Name == null) return View();
+
+            shop_context.Categories.Add(category);
+            shop_context.SaveChanges();
+            return RedirectToAction("Index", "Category");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category category_from_database = shop_context.Categories.Find(id)!;
+            if (category_from_database == null)
+            {
+                return NotFound();
+            }
+            return View(category_from_database);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString()) ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            if (ModelState.IsValid == false || category.Name == null) return View();
+
+            shop_context.Update(category);
+            shop_context.SaveChanges();
+            return RedirectToAction("Index", "Category");
+        }
+        public IActionResult Delete(int? id)
+        {
+            Category? category_from_database = shop_context.Categories.Find(id);
+            if (category_from_database == null) return NotFound();
+            
+            shop_context.Remove(category_from_database);
+            shop_context.SaveChanges();
+
+            return RedirectToAction("Index", "Category");
+        }
     }
 }
--- ASP.NET_BookShop/Models/Category.cs
+++ ASP.NET_BookShop/Models/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ASP.NET_BookShop.Models
 {
@@ -10,8 +12,12 @@ namespace ASP.NET_BookShop.Models
         [Key]
         public int Id { get; set; }
         [Column("name")]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
         [Column("display_order")]
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be in range [1, 100]")]
         public int DisplayOrder { get; set; }
     }
 }
