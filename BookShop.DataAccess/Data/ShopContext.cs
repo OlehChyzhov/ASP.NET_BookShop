f53f5fd785496cbf2401da1b08a43f590409b11a
Олег Чижов <oleh.chyzhov@lnu.edu.ua>
2024-07-22 19:58:11 +0300

    Order Confrimation beginning. Create Order Header and Details Model

--- BookShop.DataAccess/Data/ShopContext.cs
+++ BookShop.DataAccess/Data/ShopContext.cs
@@ -12,8 +12,9 @@ namespace BookShop.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<UserExtention> Users { get; set; }
         public DbSet<Company> Companies { get; set; }
-
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
